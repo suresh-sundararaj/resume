3134b191992b25917fbe900133f8c606a44d895a
suresh-sundararaj <154260794+suresh-sundararaj@users.noreply.github.com>
2023-12-18 23:10:34 +0530

    Update Resume.js

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ResumeSvg from "../assets/svg/resume_bro_1.svg";
+import SureshResume from "../assets/documents/Resume.pdf";
 
 function Resume() {
   const [hover, setHover] = React.useState(false);
@@ -19,22 +20,29 @@ function Resume() {
         <u>Resume</u>
       </h2>
       <img src={ResumeSvg} width="350px" alt="Resume illustration" />
-      <button
-        style={{
-          backgroundColor: hover ? "inherit" : "#0174BE",
-          color: hover ? "inherit" : "#FFFFFF",
-          padding: "10px",
-          boxShadow: hover
-            ? "3px 3px white, 4px 4px #0174BE"
-            : "3px 3px white, 4px 4px #0174BE",
-          cursor: hover ? "pointer" : "context-menu",
-          border: hover ? "1px solid #0174BE" : "inherit",
-        }}
-        onMouseEnter={() => setHover(true)}
-        onMouseLeave={() => setHover(false)}
+      <a
+        href={SureshResume}
+        download="Suresh Resume"
+        target="_blank"
+        rel="noreferrer"
       >
-        <b>Download & View</b>
-      </button>
+        <button
+          style={{
+            backgroundColor: hover ? "inherit" : "#0174BE",
+            color: hover ? "inherit" : "#FFFFFF",
+            padding: "10px",
+            boxShadow: hover
+              ? "3px 3px white, 4px 4px #0174BE"
+              : "3px 3px white, 4px 4px #0174BE",
+            cursor: hover ? "pointer" : "context-menu",
+            border: hover ? "1px solid #0174BE" : "1px solid #0174BE",
+          }}
+          onMouseEnter={() => setHover(true)}
+          onMouseLeave={() => setHover(false)}
+        >
+          <b>Download & View</b>
+        </button>
+      </a>
     </div>
   );
 }
